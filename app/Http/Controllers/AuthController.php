6a0be5cac2e98ade598d50e1af956dece6b772c1
swanyee <swanyeetun.2000@gmail.com>
2022-03-21 22:46:00 +0630

    add logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -34,6 +34,7 @@ class AuthController extends Controller
     }
 
     public function login(Request $request){
+
         $credentials = $request->validate([
             'email'=>'required|email|string|exists:users,email',
             'password'=>[
@@ -42,7 +43,7 @@ class AuthController extends Controller
             'remember'=>'boolean'
         ]);
         $remember = $credentials['remember'] ?? false;
-        unset($credentials);
+        unset($credentials['remember']);
 
         if(!Auth::attempt($credentials,$remember)){
             return response([
@@ -56,6 +57,15 @@ class AuthController extends Controller
         return response([
             'user' =>$user,
             'token'=> $token,
+        ]);
+    }
+
+    public function logout(){
+        $user = Auth::user();
+        $user->currentAccessToken()->delete();
+
+        return response([
+            'success'=>true
         ]);
     }
 }
